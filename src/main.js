18e58111ea06262630ed08d83dfd3cc45cb45eca
Brayan Pallares <ing.pallares69@gmail.com>
2026-05-13 11:34:03 -0600

    Cree mis funciones de resta, divion y concatenar

--- src/main.js
+++ src/main.js
@@ -1,4 +1,34 @@
 
-export function addTwoNumbers(/*aqui tambien va código*/){
+export function addTwoNumbers( c, d){
   //aqui va tu código
+  const result = c + d ;
+
+  
+ console.log(result);
 }
+addTwoNumbers(2, 3);
+
+export function subtractTwoNumbers(c, d){
+  const result = c - d;
+
+  console.log(result);
+}
+subtractTwoNumbers( 2, 3);
+
+export function dividTwoNumbers(a , b ){
+  const result = a / b;
+  console.log(result);
+}
+dividTwoNumbers(100, 5);
+
+export function sumThreeNumbers(a, b, c){
+  const result = a + b + c;
+  console.log(result);
+}
+sumThreeNumbers( 5, 10, 3);
+
+export function encaThreeNumbers(a, b, c){
+  const result = a / b + c;
+  console.log(result);
+}
+encaThreeNumbers( 150, 3, 50);
